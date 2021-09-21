f6327f9b57f65ff9d88dd16ecb17f64424458edb
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2021-09-21 14:20:41 +0300

    First version of the airbnb-clone

---
+++ src/listings.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.listings = void 0;
+exports.listings = [
+    {
+        id: '001',
+        title: 'Чистая и готовая к заселению квартира. 10 минут от Москва-Сити',
+        image: 'https://pix8.agoda.net/hotelImages/10544730/0/a62b696fb9b25bc1b298e3ff79711c31.jpg',
+        address: 'Москва',
+        price: 100000,
+        numOfGuests: 2,
+        numOfBeds: 1,
+        numOfBaths: 2,
+        rating: 5,
+        bookings: [],
+        favorite: true,
+    },
+    {
+        id: '002',
+        title: 'Luxurious home with private pool',
+        image: 'https://res.cloudinary.com/tiny-house/image/upload/v1560645376/mock/Los%20Angeles/los-angeles-listing-1_aikhx7.jpg',
+        address: '100 Hollywood Hills Dr, Los Angeles, California',
+        price: 15000,
+        numOfGuests: 2,
+        numOfBeds: 1,
+        numOfBaths: 1,
+        rating: 4,
+        bookings: [],
+        favorite: false,
+    },
+    {
+        id: '003',
+        title: 'Single bedroom located in the heart of downtown San Fransisco',
+        image: 'https://res.cloudinary.com/tiny-house/image/upload/v1560646219/mock/San%20Fransisco/san-fransisco-listing-1_qzntl4.jpg',
+        address: '200 Sunnyside Rd, San Fransisco, California',
+        price: 25000,
+        numOfGuests: 3,
+        numOfBeds: 2,
+        numOfBaths: 2,
+        rating: 3,
+        bookings: [],
+        favorite: false,
+    },
+];
